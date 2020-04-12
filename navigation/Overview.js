c8a2e78511fa8d068a21d9354ffae8010d8043bf
ikismail <ikismail7@gmail.com>
2020-04-12 15:07:28 +0530

    Removed Expo Stack

--- navigation/Overview.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createStackNavigator } from "react-navigation";
-
-import Overview from "../screens/Overview";
-import ProductDetail from "../screens/ProductDetail";
-export default createStackNavigator({
-  Overview,
-  ProductDetail
-});
